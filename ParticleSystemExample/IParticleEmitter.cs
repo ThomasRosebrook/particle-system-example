aa255f7c2694a3a981dbb052bf1704a7e8907401
ThomasRosebrook <trosebr@ksu.edu>
2024-09-27 01:57:06 -0500

    Finished Particle System Example

---
+++ ParticleSystemExample/IParticleEmitter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace ParticleSystemExample
+{
+    public interface IParticleEmitter
+    {
+        public Vector2 Position { get; }
+
+        public Vector2 Velocity { get; }
+
+    }
+}
